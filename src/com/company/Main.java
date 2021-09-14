59f9e8d3c21ec93d544c7a8d12dfddeee994ed25
DanWuPet <danx1698@edu.easj.dk>
2021-09-14 10:31:41 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int x = 5;
+        if (x > 0 && x < 10) {
+            System.out.println("positive single digit number");
+        }
+    }
+}
